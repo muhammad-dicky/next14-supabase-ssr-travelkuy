ecc31a4f53ba04f034f2b304f7ba8aed9f0cc229
Dicky <muhammad.dickynn@gmail.com>
2023-11-18 18:25:08 +0700

    Feat: Create Todo

--- app/todo/actions/index.ts
+++ app/todo/actions/index.ts
@@ -1,4 +1,16 @@
-export async function createTodo(title: string) {}
+"use server"
+
+import createSupabaseServerClient from "@/lib/supabase/server"
+
+export async function createTodo(title: string) {
+
+    const supabase = await createSupabaseServerClient();
+
+   const result = await supabase.from("todo").insert({title}).single()
+
+   return JSON.stringify(result)
+
+}
 
 export async function readTodo() {}
 
